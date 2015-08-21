8454ef610eb302f93853fda7f9f7c2d0d6f91f7e
Christian Hermann <c.hermann@bitbeans.de>
2015-08-21 07:25:51 +0200

    travis test

--- travis-osx-deps.sh
+++ travis-osx-deps.sh
@@ -8,5 +8,5 @@ wget "http://download.mono-project.com/archive/${MONO_VER}/macos-10-x86/MonoFram
 sudo installer -pkg "MonoFramework-MDK-${MONO_VER}.macos10.xamarin.x86.pkg" -target /
 
 mozroots --import --sync
-mono --runtime=v4.0 ./.nuget/NuGet.exe restore ./libsodium-net.sln
+mono --runtime=v4.0 ./.nuget/NuGet.exe restore ./minisign-net.sln
 echo "mono --runtime=v4.0  ./packages/NUnit.Runners.2.6.4/tools/nunit-console.exe \"\$@\"" > nunit-console.sh
