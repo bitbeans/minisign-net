264decb9ab41233f99273c19250b97f259bd4bba
Christian Hermann <c.hermann@bitbeans.de>
2017-01-07 19:37:33 +0100

    Update
    - libsodium-net 10.0
    - Nunit 3.5
    - 2017
    - New travis

--- Tests/BaseTests.cs
+++ Tests/BaseTests.cs
@@ -14,8 +14,8 @@ namespace Tests
         [Test]
         public void GenerateKeyTest()
         {
-            const string seckeypass = "7e725ac9f52336f74dc54bbe2912855f79baacc08b008437809fq5527f1b2256";
-            const string testfolder = "Testfolder";
+			const string seckeypass = "7e725ac9f52336f74dc54bbe2912855f79baacc08b008437809fq5527f1b2256";
+            var testfolder = Path.Combine(TestContext.CurrentContext.TestDirectory, "Testfolder");
 
             var minisignKeyPair = Minisign.GenerateKeyPair(seckeypass, true, testfolder);
             
@@ -34,8 +34,8 @@ namespace Tests
         [Test]
         public void SignTest()
         {
-            const string testfolder = "Testfolder";
-            const string expected = "9d6f33b5e347042e";
+			var testfolder = Path.Combine(TestContext.CurrentContext.TestDirectory, "Testfolder");
+			const string expected = "9d6f33b5e347042e";
             const string seckeypass = "7e725ac9f52336f74dc54bbe2912855f79baacc08b008437809fq5527f1b2256";
             const string privateKey = "456453634232aeb543fbea3467ad996ac237b38646bcbc12e6232fbc0a8cd9a1ed46c7263af200000002000000000000004000000000992f22d875591d3bb7dc3f77caba3229e2f7b8afe655140bafabcb6c5d8b259366a2897624de65743de71f8f2dcc545a96c4b530ffd796d92f35eb02425f4196ab9a37ff2f542774d676625f8de689fa2da3e0a0250efd58347c35b927ca49ec4d93687be59d6e1a";
             var minisignPrivateKey = Minisign.LoadPrivateKey(Utilities.HexToBinary(privateKey), Encoding.UTF8.GetBytes(seckeypass));
@@ -55,8 +55,8 @@ namespace Tests
         [Test]
         public void Sign2Test()
         {
-            const string testfolder = "Testfolder";
-            const string expected = "9d6f33b5e347042e";
+			var testfolder = Path.Combine(TestContext.CurrentContext.TestDirectory, "Testfolder");
+			const string expected = "9d6f33b5e347042e";
             const string seckeypass = "7e725ac9f52336f74dc54bbe2912855f79baacc08b008437809fq5527f1b2256";
             const string privateKey = "456453634232aeb543fbea3467ad996ac237b38646bcbc12e6232fbc0a8cd9a1ed46c7263af200000002000000000000004000000000992f22d875591d3bb7dc3f77caba3229e2f7b8afe655140bafabcb6c5d8b259366a2897624de65743de71f8f2dcc545a96c4b530ffd796d92f35eb02425f4196ab9a37ff2f542774d676625f8de689fa2da3e0a0250efd58347c35b927ca49ec4d93687be59d6e1a";
             var minisignPrivateKey = Minisign.LoadPrivateKey(Utilities.HexToBinary(privateKey), Encoding.UTF8.GetBytes(seckeypass));
@@ -90,8 +90,8 @@ namespace Tests
         public void LoadSignatureFromFileTest()
         {
             const string expected = "9d6f33b5e347042e";
-            const string testfolder = "Testfolder";
-            var file = Path.Combine(testfolder, "test.jpg.minisig");
+			var testfolder = Path.Combine(TestContext.CurrentContext.TestDirectory, "Testfolder");
+			var file = Path.Combine(testfolder, "test.jpg.minisig");
             var minisignSignature = Minisign.LoadSignatureFromFile(file);
             Assert.AreEqual(expected, Utilities.BinaryToHex(minisignSignature.KeyId));
         }
@@ -108,8 +108,8 @@ namespace Tests
         public void LoadPublicKeyFromFileTest()
         {
             const string expected = "9d6f33b5e347042e";
-            const string testfolder = "Testfolder";
-            var file = Path.Combine(testfolder, "test.pub");
+			var testfolder = Path.Combine(TestContext.CurrentContext.TestDirectory, "Testfolder");
+			var file = Path.Combine(testfolder, "test.pub");
             var minisignPublicKey = Minisign.LoadPublicKeyFromFile(file);
             Assert.AreEqual(expected, Utilities.BinaryToHex(minisignPublicKey.KeyId));
         }
@@ -141,8 +141,8 @@ namespace Tests
             const string seckeypass = "7e725ac9f52336f74dc54bbe2912855f79baacc08b008437809fq5527f1b2256";
             const string expected =
                 "521437eb06d390e3881d6227543c670bd79ce4092845a4d567e85013c6ffe454387c964068069a7f501835a5df4d8283d917ecba652b048dabe883af1a35f305";
-            const string testfolder = "Testfolder";
-            var file = Path.Combine(testfolder, "test.key");
+			var testfolder = Path.Combine(TestContext.CurrentContext.TestDirectory, "Testfolder");
+			var file = Path.Combine(testfolder, "test.key");
             var minisignPrivateKey = Minisign.LoadPrivateKeyFromFile(file, seckeypass);
             Assert.AreEqual(expected, Utilities.BinaryToHex(minisignPrivateKey.SecretKey));
         }
